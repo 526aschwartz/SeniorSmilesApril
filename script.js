8cbe4a07959c12c4323b41190c399688951bb133
526aschwartz <526aschwartz@frhsd.com>
2025-04-25 10:43:32 -0400

    added stuff

---
+++ script.js
@@ -0,0 +1,32 @@
+const images = [
+    'imgs/bsket.JPG',
+    'imgs/card.JPG',
+    'imgs/st.JPG',
+    'imgs/val.JPG'
+];
+
+// Function to generate a random image
+function generateRandomImage() {
+    // Randomly select an image index
+    const randomIndex = Math.floor(Math.random() * images.length);
+
+    // Get the image container div
+    const imageContainer = document.getElementById("imageContainer");
+
+    // If an image is already displayed, remove it
+    if (imageContainer.innerHTML !== "") {
+        imageContainer.innerHTML = "";
+    }
+
+    // Create an image element
+    const imgElement = document.createElement("img");
+    imgElement.src = images[randomIndex]; 
+    imgElement.alt = "Random Image"; 
+    imgElement.classList.add("img-fluid"); 
+
+    // Add the image to the container
+    imageContainer.appendChild(imgElement);
+}
+
+// 3 seconds
+setInterval(generateRandomImage, 3000);
